6ad022e782218cc4b715635ab4f3148da6e16977
Furentsu <eugeniofranzoso@gmail.com>
2021-08-30 17:21:22 +0200

    Added coupon functionality and imported bootstrap v5

--- js/script.js
+++ js/script.js
@@ -9,12 +9,19 @@ if (!nomePanino) {
 let bottone = document.getElementById("calculator-button");
 let listaIngredienti = document.getElementsByClassName("ingredients");
 
+let discount = document.getElementById("discount-code");
+let discountCodes = ["SUMMERSALES", "FALLSALES", "WINTERSALES", "SPRINGSALES"];
+
 bottone.addEventListener("click", function() {
     let prezzoPanino = 50;
     for (let i = 0; i < listaIngredienti.length; i++) {
         if (listaIngredienti[i].checked) {
             prezzoPanino += 10;
-        }   
+        } 
+    }   
+    
+    if (discountCodes.includes(discount.value)) {
+        prezzoPanino -= Math.floor(((prezzoPanino * 20) / 100));
     }
     document.getElementById("final-price").innerHTML = prezzoPanino;
 })
